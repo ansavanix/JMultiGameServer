250c376c8b849851e8870453bce07fcc748f2e94
Anthony Saldana Valle <55422282+ansavanix@users.noreply.github.com>
2023-05-30 18:36:18 -0700

    Modify build script

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ javac multigame/server/Server.java
 javac multigame/client/Client.java
 
 jar -cfm Server.jar ServerManifest.txt multigame/server/*.class multigame/shared/*.class multigame/games/*.class
-jar -cfm Client.jar ClientManifest.txt multigame/client/*.class multigame/shared/*.class
+jar -cfm Client.jar ClientManifest.txt multigame/client/*.class multigame/shared/*.class multigame/games/Game.class
